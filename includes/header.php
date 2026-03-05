390b746003ae61806723b671bc4e6d0c1d72b26f
Carlosmveloso <carloseduardomveloso@gmail.com>
2026-03-05 11:53:06 -0300

    continuando na organização das pastas do projeto

---
+++ includes/header.php
@@ -0,0 +1,86 @@
+    <header>
+      <div class="d-flex justify-content-between align-items-center px-3 pt-2">
+        <div class="d-flex justify-content-start gap-3 w-25">
+          <a href=""
+            ><img class="icons" src="assets/img/banner-1.jpg" alt=""
+          /></a>
+          <a href=""
+            ><img class="icons" src="images/tiktok-icon.png" alt=""
+          /></a>
+          <a href=""
+            ><img class="icons" src="images/facebook-icon.png" alt=""
+          /></a>
+          <a href=""
+            ><img class="icons" src="images/linkedin-logo.png" alt=""
+          /></a>
+          <a href=""
+            ><img class="icons" src="images/youtube-icon.png" alt=""
+          /></a>
+        </div>
+
+        <div class="d-flex justify-content-center w-50">
+          <a href=""
+            ><img
+              class="logo-marca"
+              src="images/logo-marca.png"
+              alt="Logo da Marca"
+          /></a>
+        </div>
+
+        <div class="d-flex justify-content-end gap-3 w-25">
+          <a href=""><img src="images/coracao-icon.png" alt="" /></a>
+          <a href=""><img src="images/conta-icon.png" alt="" /></a>
+          <a href=""><img src="images/lupa-icon.png" alt="" /></a>
+          <a href=""><img src="images/sacola-icon.png" alt="" /></a>
+        </div>
+      </div>
+
+      <!-- ✅ MENU ATUALIZADO COM DROPDOWN -->
+      <nav class="d-flex gap-5 justify-content-center flex-wrap mt-5">
+        <div class="menu-item">
+          <a href="#" class="menu__link">Lançamento</a>
+          <div class="submenu">
+            <a href="#">Camisetas</a>
+            <a href="#">Calças</a>
+            <a href="#">Bermudas</a>
+            <a href="#">Acessórios</a>
+          </div>
+        </div>
+        <div class="menu-item">
+          <a href="pages/masculino.html" class="menu__link">Masculino</a>
+          <div class="submenu">
+            <a href="#">Camisetas</a>
+            <a href="#">Calças</a>
+            <a href="#">Bermudas</a>
+            <a href="#">Acessórios</a>
+          </div>
+        </div>
+        <div class="menu-item">
+          <a href="#" class="menu__link">Feminino</a>
+          <div class="submenu">
+            <a href="#">Blusas</a>
+            <a href="#">Vestidos</a>
+            <a href="#">Saias</a>
+            <a href="#">Moda Fitness</a>
+          </div>
+        </div>
+        <div class="menu-item">
+          <a href="#" class="menu__link">Destaques</a>
+          <div class="submenu">
+            <a href="#">Camisetas</a>
+            <a href="#">Calças</a>
+            <a href="#">Bermudas</a>
+            <a href="#">Acessórios</a>
+          </div>
+        </div>
+        <div class="menu-item">
+          <a href="#" class="menu__link">Promoção</a>
+          <div class="submenu">
+            <a href="#">Camisetas</a>
+            <a href="#">Calças</a>
+            <a href="#">Bermudas</a>
+            <a href="#">Acessórios</a>
+          </div>
+        </div>
+      </nav>
+    </header>
